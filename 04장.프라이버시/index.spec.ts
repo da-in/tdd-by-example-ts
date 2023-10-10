0856011ccd60c50d474c9f5fc90a9c7c184e691d
hanjiwon1 <wldnjs20725@gmail.com>
2023-10-10 14:17:56 +0900

    Example p.66-2

--- 04장.프라이버시/index.spec.ts
+++ 04장.프라이버시/index.spec.ts
@@ -4,7 +4,7 @@ describe('화폐 예제', () => {
   it('testMultiplication', () => {
     const five: Dollar = new Dollar(5)
     let product: Dollar = five.times(2)
-    expect(product.amount).toBe(10)
+    expect(product).toMatchObject(new Dollar(10))
     product = five.times(3)
     expect(product.amount).toBe(15)
   })
