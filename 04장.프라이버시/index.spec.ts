097f407b999feee4ae7876056426fad4671efafe
hanjiwon1 <wldnjs20725@gmail.com>
2023-10-10 14:13:00 +0900

    Example p.66-1

---
+++ 04장.프라이버시/index.spec.ts
@@ -0,0 +1,16 @@
+import { Dollar } from './index'
+
+describe('화폐 예제', () => {
+  it('testMultiplication', () => {
+    const five: Dollar = new Dollar(5)
+    let product: Dollar = five.times(2)
+    expect(product.amount).toBe(10)
+    product = five.times(3)
+    expect(product.amount).toBe(15)
+  })
+
+  it('testEquality', () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
+    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
+  })
+})
