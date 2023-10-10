e1cf67749887a78014fa2e05625c0fbdc5394787
hanjiwon1 <wldnjs20725@gmail.com>
2023-10-10 14:10:34 +0900

    Example p.62-1

--- 03장. 모두를 위한 평등/index.spec.ts
+++ 03장. 모두를 위한 평등/index.spec.ts
@@ -11,5 +11,6 @@ describe('화폐 예제', () => {
 
   it('testEquality', () => {
     expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
+    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
   })
 })
