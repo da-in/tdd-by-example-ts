f2c1445bedededfa6d6d44ba9cf3049ead568a06
hanjiwon1 <wldnjs20725@gmail.com>
2023-10-10 13:59:42 +0900

    Example p.61-1

---
+++ 03장. 모두를 위한 평등/index.ts
@@ -0,0 +1,14 @@
+export class Dollar {
+  amount: number
+  constructor (amount: number) {
+    this.amount = amount
+  }
+
+  times (multiplier: number) {
+    return new Dollar(this.amount * multiplier)
+  }
+
+  equals (object: object) {
+    return true
+  }
+}
