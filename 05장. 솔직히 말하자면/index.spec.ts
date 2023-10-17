687d8d5ee45aaa2aa2be98076ded91f53f1ee727
정다현 <dh45@wavve.com>
2023-10-17 13:44:25 +0900

    Chp.5 initial commit

---
+++ 05장. 솔직히 말하자면/index.spec.ts
@@ -0,0 +1,14 @@
+import { Dollar } from './index'
+
+describe('화폐 예제', () => {
+  it('testMultiplication', () => {
+    const five: Dollar = new Dollar(5)
+    expect(five.times(2)).toMatchObject(new Dollar(10))
+    expect(five.times(3)).toMatchObject(new Dollar(15))
+  })
+
+  it('testEquality', () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
+    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
+  })
+})
