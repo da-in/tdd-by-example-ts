ba4d89d58529bee6dc4523898248916f10ec4af9
정다현 <dh45@wavve.com>
2023-10-17 13:46:02 +0900

    Example p.70

--- 05장. 솔직히 말하자면/index.spec.ts
+++ 05장. 솔직히 말하자면/index.spec.ts
@@ -11,4 +11,10 @@ describe('화폐 예제', () => {
     expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
     expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
   })
+
+  it('testFrancMultiplication', () => {
+    const five: Franc = new Franc(5)
+    expect(five.times(2)).toMatchObject(new Franc(10))
+    expect(five.times(3)).toMatchObject(new Franc(15))
+  })
 })
