ca9c78981193b980f439ff52cdec72491c7658aa
정다현 <dh45@wavve.com>
2023-10-17 13:52:36 +0900

    Chp.6 initial commit

---
+++ 06장. 돌아온 '모두를 위한 평등'/index.ts
@@ -0,0 +1,31 @@
+export class Dollar {
+  #amount: number
+  constructor (amount: number) {
+    this.#amount = amount
+  }
+
+  times (multiplier: number) {
+    return new Dollar(this.#amount * multiplier)
+  }
+
+  equals (object: object) {
+    const dollar = object as Dollar
+    return this.#amount === dollar.#amount
+  }
+}
+
+export class Franc {
+  #amount: number
+  constructor (amount: number) {
+    this.#amount = amount
+  }
+
+  times (multiplier: number) {
+    return new Franc(this.#amount * multiplier)
+  }
+
+  equals (object: object) {
+    const franc = object as Franc
+    return this.#amount === franc.#amount
+  }
+}
