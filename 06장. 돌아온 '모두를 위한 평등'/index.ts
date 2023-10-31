9beabfbc3ecd2bbc2e37825dae8cf27121382230
3people <smsn7031@naver.com>
2023-10-31 13:55:17 +0900

    Example p.79-3

--- 06장. 돌아온 '모두를 위한 평등'/index.ts
+++ 06장. 돌아온 '모두를 위한 평등'/index.ts
@@ -23,17 +23,17 @@ export class Dollar extends Money {
 }
 
 export class Franc extends Money {
-  #amount: number
   constructor (amount: number) {
-    this.#amount = amount
+    super(amount)
+    this._amount = amount
   }
 
   times (multiplier: number) {
-    return new Franc(this.#amount * multiplier)
+    return new Franc(this._amount * multiplier)
   }
 
   equals (object: object) {
     const franc = object as Franc
-    return this.#amount === franc.#amount
+    return this._amount === franc._amount
   }
 }
