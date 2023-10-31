bd600e6ab761c7f801455740d5944d67587af1e7
3people <smsn7031@naver.com>
2023-10-31 13:52:44 +0900

    Example p.79-1

--- 06장. 돌아온 '모두를 위한 평등'/index.spec.ts
+++ 06장. 돌아온 '모두를 위한 평등'/index.spec.ts
@@ -10,6 +10,8 @@ describe('화폐 예제', () => {
   it('testEquality', () => {
     expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
     expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
+    expect(new Franc(5).equals(new Franc(5))).toBeTruthy()
+    expect(new Franc(5).equals(new Franc(6))).toBeFalsy()
   })
 
   it('testFrancMultiplication', () => {
