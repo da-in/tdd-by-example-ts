386d4b54e49cc1761640c3bb292cd4755e85259f
da-in <talown@naver.com>
2023-11-07 14:07:57 +0900

    Example p.91-2

--- 07장. 사과와 오렌지/index.ts
+++ 07장. 사과와 오렌지/index.ts
@@ -9,6 +9,10 @@ export abstract class Money {
     return new Dollar(amount)
   }
 
+  static franc (amount: number): Money {
+    return new Franc(amount)
+  }
+
   abstract times (multiplier: number): Money
 
   equals (object: object) {
