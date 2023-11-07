b2189217981d7cfe13521a6270167efb3b4de872
da-in <talown@naver.com>
2023-11-07 14:04:11 +0900

    Example p.90-2

--- 07장. 사과와 오렌지/index.spec.ts
+++ 07장. 사과와 오렌지/index.spec.ts
@@ -1,18 +1,18 @@
-import { Dollar, Franc, Money } from './index'
+import { Franc, Money } from './index'
 
 describe('화폐 예제', () => {
   it('testMultiplication', () => {
     const five: Money = Money.dollar(5)
-    expect(five.times(2)).toMatchObject(new Dollar(10))
-    expect(five.times(3)).toMatchObject(new Dollar(15))
+    expect(five.times(2)).toMatchObject(Money.dollar(10))
+    expect(five.times(3)).toMatchObject(Money.dollar(15))
   })
 
   it('testEquality', () => {
-    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
-    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy()
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy()
     expect(new Franc(5).equals(new Franc(5))).toBeTruthy()
     expect(new Franc(5).equals(new Franc(6))).toBeFalsy()
-    expect(new Franc(5).equals(new Dollar(5))).toBeFalsy()
+    expect(new Franc(5).equals(Money.dollar(5))).toBeFalsy()
   })
 
   it('testFrancMultiplication', () => {
