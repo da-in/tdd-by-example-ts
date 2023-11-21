5992fb843cc85359bfa34589e58e6fa59fe1aade
hanjiwon1 <wldnjs20725@gmail.com>
2023-11-21 14:10:03 +0900

    Chp.10 initial commit

---
+++ 10장. 흥미로운 시간/index.ts
@@ -0,0 +1,60 @@
+export abstract class Money {
+  protected _amount: number
+  protected _currency: string
+
+  protected constructor (amount: number, currency: string) {
+    this._amount = amount
+    this._currency = currency
+  }
+
+  static dollar (amount: number): Money {
+    return new Dollar(amount, 'USD')
+  }
+
+  static franc (amount: number): Money {
+    return new Franc(amount, 'CHF')
+  }
+
+  currency (): string {
+    return this._currency
+  }
+
+  abstract times (multiplier: number): Money
+
+  equals (object: object) {
+    const money: Money = object as Money
+    return this._amount === money._amount && money instanceof this.constructor
+  }
+}
+
+export class Dollar extends Money {
+  constructor (amount: number, currency: string) {
+    super(amount, currency)
+    this._amount = amount
+    this._currency = currency
+  }
+
+  currency (): string {
+    return this._currency
+  }
+
+  times (multiplier: number): Money {
+    return Money.dollar(this._amount * multiplier)
+  }
+}
+
+export class Franc extends Money {
+  constructor (amount: number, currency: string) {
+    super(amount, currency)
+    this._amount = amount
+    this._currency = currency
+  }
+
+  currency (): string {
+    return this._currency
+  }
+
+  times (multiplier: number): Money {
+    return Money.franc(this._amount * multiplier)
+  }
+}
