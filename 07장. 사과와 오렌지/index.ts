82394f4d1a5fa8b11b919d0bfda743f949158a9b
hanjiwon1 <wldnjs20725@gmail.com>
2023-11-21 14:01:37 +0900

    Example p.97-2

--- 07장. 사과와 오렌지/index.ts
+++ 07장. 사과와 오렌지/index.ts
@@ -11,7 +11,7 @@ export abstract class Money {
   }
 
   static franc (amount: number): Money {
-    return new Franc(amount, null)
+    return new Franc(amount, 'CHF')
   }
 
   currency (): string {
