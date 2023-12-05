9bb805dfb5b3b64c3236459badc86eddeca15493
da-in <talown@naver.com>
2023-12-05 14:05:15 +0900

    Chp.12 initial commit

---
+++ 12장. 드디어, 더하기/index.spec.ts
@@ -0,0 +1,20 @@
+import { Money } from './index'
+
+describe('화폐 예제', () => {
+  it('testMultiplication', () => {
+    const five: Money = Money.dollar(5)
+    expect(five.times(2)).toMatchObject(Money.dollar(10))
+    expect(five.times(3)).toMatchObject(Money.dollar(15))
+  })
+
+  it('testEquality', () => {
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy()
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy()
+    expect(Money.franc(5).equals(Money.dollar(5))).toBeFalsy()
+  })
+
+  it('testCurrency', () => {
+    expect(Money.dollar(1).currency()).toBe('USD')
+    expect(Money.franc(1).currency()).toBe('CHF')
+  })
+})
