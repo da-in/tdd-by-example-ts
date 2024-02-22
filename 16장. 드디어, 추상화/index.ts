592baa32e7849dbfd813c44ecf1c96573c27dc42
da-in <talown@naver.com>
2024-02-22 13:22:08 +0900

    Example p.146-2

--- 16장. 드디어, 추상화/index.ts
+++ 16장. 드디어, 추상화/index.ts
@@ -2,6 +2,8 @@ export interface Expression {
   reduce: (bank: Bank, to: string) => Money
 
   plus: (addend: Expression) => Expression
+
+  times: (multiplier: number) => Expression
 }
 
 export class Bank {
