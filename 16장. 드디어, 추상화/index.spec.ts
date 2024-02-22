984f30bf99e69606d5e0b3fec9ec57b874f2beed
da-in <talown@naver.com>
2024-02-22 13:36:01 +0900

    Example p.148

--- 16장. 드디어, 추상화/index.spec.ts
+++ 16장. 드디어, 추상화/index.spec.ts
@@ -89,9 +89,4 @@ describe('화폐 예제', () => {
     const result = bank.reduce(sum, 'USD')
     expect(Money.dollar(20)).toMatchObject(result)
   })
-
-  it('testPlusSameCurrencyReturnsMoney', () => {
-    const sum: Expression = Money.dollar(1).plus(Money.dollar(1))
-    expect(sum instanceof Money).toBeTruthy()
-  })
 })
